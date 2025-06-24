e1506aac55ea8c9f3e10daeb88b8db4809cff5e4
Gaurav Agarwalla <agarwalgaurav1809@gmail.com>
2025-06-24 16:00:59 +0530

    change

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import cors from "cors";
 import path from "path";
+import http from "http";
+import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
+import { initSocket } from "./lib/socket.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001 ;
+const app = express();
+const server = http.createServer(app);  // keep 1 app instance
+initSocket(server); // start socket server here
+
 const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
